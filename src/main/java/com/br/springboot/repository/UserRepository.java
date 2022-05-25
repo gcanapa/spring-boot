fd3afc951ee6365b045658605ad0568e079cf46f
Gustavo Canapa <gustavo.s.canapa@gmail.com>
2022-05-25 15:10:50 -0300

    Simple application using spring boot

---
+++ src/main/java/com/br/springboot/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.br.springboot.repository;
+
+import java.util.List;
+
+import com.br.springboot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+    @Query("SELECT u from User u where u.id > :id")
+    public List<User> findAllMoreThan(@Param("id") Long id);
+
+    //Aqui ele está fazendo o mesmo select que a query de cima 
+    public List<User> findByIdGreaterThan(Long id);
+    //Aqui está fazendo um select baseado no nome passado na url
+    public List<User> findByNameIgnoreCase(String name);
+}
